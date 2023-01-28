b259efcb085fc566d881cc891f0985a8d80b32e8
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-01-28 19:35:45 +0100

    Improved ram fetching script, added disk usage script

--- monitoring.sh
+++ monitoring.sh
@@ -3,11 +3,13 @@
 arc=$(uname -a)
 pcpu=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
 vcpu=$(lscpu -e=CPU | sed '/CPU/d' | wc -l)
-uram=$(free -m | sed -n '2p' | awk '{ print $3 }')
-tram=$(free -m | awk '$1 == "Mem:" { print $2 }')
+ram=$(free -m | awk '/^Mem/ { printf "%s/%sMB (%.2f%%)", $3, $2, $3*100/$2 }')
+disk=$(df -Bm | grep -v /boot$ | awk '/^\/dev/ { ut+=$3 ; tt+=$2 } \
+	END { printf "%s/%dGb (%.0f%%)", ut, tt/1024, ut*100/tt }')
 
 wall "	#Architecture: $arc
 	#CPU physical: $pcpu
 	#vCPU: $vcpu
-	#Memory Usage: $uram/${tram}MB
-	"
+	#Memory Usage: $ram
+	#Disk Usage: $disk
+     "
