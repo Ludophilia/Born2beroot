40f1983a7820a8599be6ac3f5ec7c01163347424
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-01-28 16:04:45 +0100

    Added commands to fetch info about number of physical processors, used memory, available memory

--- monitoring.sh
+++ monitoring.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-wall "	#Architecture: $(uname -a)
-		#CPU physical: $()
-		#vCPU: $(lscpu -e=CPU | sed '/CPU/d' | wc -l)
-    "
+arc=$(uname -a)
+pcpu=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
+vcpu=$(lscpu -e=CPU | sed '/CPU/d' | wc -l)
+uram=$(free -m | sed -n '2p' | awk '{ print $3 }')
+tram=$(free -m | awk '$1 == "Mem:" { print $2 }')
+
+wall "	#Architecture: $arc
+	#CPU physical: $pcpu
+	#vCPU: $vcpu
+	#Memory Usage: $uram/${tram}MB
+	"
